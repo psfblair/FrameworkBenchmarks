4db4f378b42837923c7df2356b20de865de957e0
Paul Blair <psfblair@gmail.com>
2015-07-29 16:05:16 -0400

    Use client-server application template and sitelets so that we can do both OWIN and IIS hosting.

--- frameworks/FSharp/websharper-sqlprovider/Src/Router.fs
+++ frameworks/FSharp/websharper-sqlprovider/Src/Router.fs
@@ -19,27 +19,28 @@ type Endpoints =
     | [<EndPoint "GET /updates">]   DataUpdateBadParam of badParameter: string
 
 
-let BenchmarksApplication =
-    let random = System.Random()
+type BenchmarksApplication() =
+    interface IWebsite<Endpoints> with
+        member this.Sitelet = 
+            let random = System.Random()
 
-    Warp.CreateApplication (fun ctx endpoint ->
-        match endpoint with
-        | Endpoints.Plaintext   -> Hello.plaintextContent |> CustomContentAsync 
-        | Endpoints.Json        -> Hello.jsonContent      |> JsonContentAsync 
-        | Endpoints.Fortunes    -> Fortune.fortuneContent |> Content.WithTemplateAsync Fortunes.View.fortuneTemplate 
+            Sitelet.Infer <| function
+                | Endpoints.Plaintext   -> Hello.plaintextContent |> CustomContentAsync 
+                | Endpoints.Json        -> Hello.jsonContent      |> JsonContentAsync 
+                | Endpoints.Fortunes    -> Fortune.fortuneContent |> Content.WithTemplateAsync Fortunes.View.fortuneTemplate 
 
-        | Endpoints.SingleQuery -> World.singleQueryContent random |> JsonContentAsync
+                | Endpoints.SingleQuery -> World.singleQueryContent random |> JsonContentAsync
 
-        | Endpoints.MultipleQuery (numberOfQueries)
-                                -> World.multipleQueryContent random numberOfQueries  |> JsonContentAsync
-        | Endpoints.MultipleQueryNoParam | Endpoints.MultipleQueryBadParam (_) 
-                                -> World.multipleQueryContent random 1  |> JsonContentAsync
-        | Endpoints.DataUpdate    (numberOfQueries)  
-                                -> World.multipleUpdateContent random numberOfQueries |> JsonContentAsync
-        | Endpoints.DataUpdateNoParam | Endpoints.DataUpdateBadParam (_)
-                                -> World.multipleUpdateContent random 1 |> JsonContentAsync
-    )
+                | Endpoints.MultipleQuery (numberOfQueries)
+                                        -> World.multipleQueryContent random numberOfQueries  |> JsonContentAsync
+                | Endpoints.MultipleQueryNoParam | Endpoints.MultipleQueryBadParam (_) 
+                                        -> World.multipleQueryContent random 1  |> JsonContentAsync
+                | Endpoints.DataUpdate    (numberOfQueries)  
+                                        -> World.multipleUpdateContent random numberOfQueries |> JsonContentAsync
+                | Endpoints.DataUpdateNoParam | Endpoints.DataUpdateBadParam (_)
+                                        -> World.multipleUpdateContent random 1 |> JsonContentAsync
 
+        member this.Actions = []
 
-[<EntryPoint>]
-do Warp.RunAndWaitForInput (BenchmarksApplication, false, "http://127.0.0.1:9000") |> ignore
+[<assembly: WebsiteAttribute(typeof<BenchmarksApplication>)>] 
+do ()
