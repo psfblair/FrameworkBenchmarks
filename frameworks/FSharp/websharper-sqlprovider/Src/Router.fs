a202b2b0c649a9c436a730edfb1ebd0954c06778
Paul Blair <psfblair@gmail.com>
2015-07-30 22:16:08 -0400

    Changes for running as a hosted satellite.

--- frameworks/FSharp/websharper-sqlprovider/Src/Router.fs
+++ frameworks/FSharp/websharper-sqlprovider/Src/Router.fs
@@ -1,46 +1,48 @@
-﻿module Benchmarks.WebSharper.SqlProvider.Router
+﻿namespace Benchmarks.WebSharper.SqlProvider
 
-open Benchmarks.WebSharper.SqlProvider
-open WebSharper
-open WebSharper.Sitelets
-open WebSharper.Sitelets.Content
-open System
+module Router =
 
-type Endpoints =
-    | [<EndPoint "GET /plaintext">] Plaintext
-    | [<EndPoint "GET /json">]      Json
-    | [<EndPoint "GET /fortunes">]  Fortunes
-    | [<EndPoint "GET /db">]        SingleQuery
-    | [<EndPoint "GET /queries">]   MultipleQuery of numberOfQueries: int
-    | [<EndPoint "GET /queries">]   MultipleQueryNoParam
-    | [<EndPoint "GET /queries">]   MultipleQueryBadParam of badParameter: string
-    | [<EndPoint "GET /updates">]   DataUpdate of numberOfQueries: int
-    | [<EndPoint "GET /updates">]   DataUpdateNoParam
-    | [<EndPoint "GET /updates">]   DataUpdateBadParam of badParameter: string
+    open Benchmarks.WebSharper.SqlProvider
+    open WebSharper
+    open WebSharper.Sitelets
+    open WebSharper.Sitelets.Content
+    open System
 
+    type Endpoints =
+        | [<EndPoint "GET /plaintext">] Plaintext
+        | [<EndPoint "GET /json">]      Json
+        | [<EndPoint "GET /fortunes">]  Fortunes
+        | [<EndPoint "GET /db">]        SingleQuery
+        | [<EndPoint "GET /queries">]   MultipleQuery of numberOfQueries: int
+        | [<EndPoint "GET /queries">]   MultipleQueryNoParam
+        | [<EndPoint "GET /queries">]   MultipleQueryBadParam of badParameter: string
+        | [<EndPoint "GET /updates">]   DataUpdate of numberOfQueries: int
+        | [<EndPoint "GET /updates">]   DataUpdateNoParam
+        | [<EndPoint "GET /updates">]   DataUpdateBadParam of badParameter: string
 
-type BenchmarksApplication() =
-    interface IWebsite<Endpoints> with
-        member this.Sitelet = 
-            let random = System.Random()
-
+    let random = System.Random()
+    let BenchmarksSitelet =  
             Sitelet.Infer <| function
-                | Endpoints.Plaintext   -> Hello.plaintextContent |> CustomContentAsync 
-                | Endpoints.Json        -> Hello.jsonContent      |> JsonContentAsync 
-                | Endpoints.Fortunes    -> Fortune.fortuneContent |> Content.WithTemplateAsync Fortunes.View.fortuneTemplate 
+                    | Endpoints.Plaintext   -> Hello.plaintextContent |> CustomContentAsync 
+                    | Endpoints.Json        -> Hello.jsonContent      |> JsonContentAsync 
+                    | Endpoints.Fortunes    -> Fortune.fortuneContent |> Content.WithTemplateAsync Fortunes.View.fortuneTemplate 
 
-                | Endpoints.SingleQuery -> World.singleQueryContent random |> JsonContentAsync
+                    | Endpoints.SingleQuery -> World.singleQueryContent random |> JsonContentAsync
 
-                | Endpoints.MultipleQuery (numberOfQueries)
-                                        -> World.multipleQueryContent random numberOfQueries  |> JsonContentAsync
-                | Endpoints.MultipleQueryNoParam | Endpoints.MultipleQueryBadParam (_) 
-                                        -> World.multipleQueryContent random 1  |> JsonContentAsync
-                | Endpoints.DataUpdate    (numberOfQueries)  
-                                        -> World.multipleUpdateContent random numberOfQueries |> JsonContentAsync
-                | Endpoints.DataUpdateNoParam | Endpoints.DataUpdateBadParam (_)
-                                        -> World.multipleUpdateContent random 1 |> JsonContentAsync
+                    | Endpoints.MultipleQuery (numberOfQueries)
+                                            -> World.multipleQueryContent random numberOfQueries  |> JsonContentAsync
+                    | Endpoints.MultipleQueryNoParam | Endpoints.MultipleQueryBadParam (_) 
+                                            -> World.multipleQueryContent random 1  |> JsonContentAsync
+                    | Endpoints.DataUpdate    (numberOfQueries)  
+                                            -> World.multipleUpdateContent random numberOfQueries |> JsonContentAsync
+                    | Endpoints.DataUpdateNoParam | Endpoints.DataUpdateBadParam (_)
+                                            -> World.multipleUpdateContent random 1 |> JsonContentAsync
 
-        member this.Actions = []
 
-[<assembly: WebsiteAttribute(typeof<BenchmarksApplication>)>] 
-do ()
+    type BenchmarksApplication() =
+        interface IWebsite<Endpoints> with
+            member this.Sitelet = BenchmarksSitelet
+            member this.Actions = []
+
+    [<assembly: WebsiteAttribute(typeof<BenchmarksApplication>)>] 
+    do ()
