945ce58132d63509ebfd975c28a274ec4a6a2236
Paul Blair <psfblair@gmail.com>
2015-08-02 15:04:39 -0400

    Passing tests. Now composing front-end and back-end Async

--- frameworks/FSharp/websharper-warp-sqlprovider/Src/Router.fs
+++ frameworks/FSharp/websharper-warp-sqlprovider/Src/Router.fs
@@ -1,9 +1,11 @@
 ﻿module Benchmarks.WebSharper.SqlProvider.Router
 
 open Benchmarks.WebSharper.SqlProvider
+open Benchmarks.WebSharper.SqlProvider.Fortunes.View
 open WebSharper
 open WebSharper.Sitelets
 open WebSharper.Sitelets.Content
+open WebSharper.Sitelets.ActionEncoding
 open System
 
 type Endpoints =
@@ -11,34 +13,40 @@ type Endpoints =
     | [<EndPoint "GET /json">]      Json
     | [<EndPoint "GET /fortunes">]  Fortunes
     | [<EndPoint "GET /db">]        SingleQuery
-    | [<EndPoint "GET /queries">]   MultipleQuery of numberOfQueries: int
-    | [<EndPoint "GET /queries">]   MultipleQueryNoParam
-    | [<EndPoint "GET /queries">]   MultipleQueryBadParam of badParameter: string
-    | [<EndPoint "GET /updates">]   DataUpdate of numberOfQueries: int
-    | [<EndPoint "GET /updates">]   DataUpdateNoParam
-    | [<EndPoint "GET /updates">]   DataUpdateBadParam of badParameter: string
+    | [<EndPoint "GET /queries">][<Query("queries")>]   MultipleQuery of queries: int
+    | [<EndPoint "GET /updates">][<Query("updates")>]   DataUpdate of updates: int
+
+let toPage (pageContent: ContentForTemplate) = Content.Page(Doctype = pageContent.Doctype, 
+                                                            Title = pageContent.Title, 
+                                                            Body = pageContent.Body)
+
+let (>>=) (wrapped: Async<'a>) (wrapper: 'a -> Async<'b>) =
+    async { let! contents = wrapped 
+            return! wrapper contents }
 
 [<Website>]
 let BenchmarksApplication =
     let random = System.Random()
 
-    Application.MultiPage <| fun ctx -> function
-        | Endpoints.Plaintext   -> Hello.plaintextContent |> Content.Text 
-        | Endpoints.Json        -> Hello.jsonContent |> Content.Json
-        | Endpoints.Fortunes    -> let pageContent = Fortune.fortuneContent
-                                   Content.Page(Doctype = pageContent.Doctype, Title = pageContent.Title, Body = pageContent.Body)
+    Sitelet.InferWithCustomErrors <| fun ctx -> function
+        | Success Endpoints.Plaintext   -> Hello.plaintextContent |> Content.Text 
+        | Success Endpoints.Json        -> Hello.jsonContent |> Content.Json
+        | Success Endpoints.Fortunes    -> Fortune.fortuneContent |> toPage
 
-        | Endpoints.SingleQuery                     -> World.singleQueryContent random |> Content.Json
+        | Success Endpoints.SingleQuery -> World.singleQueryContent random |> Content.Json
 
-        | Endpoints.MultipleQuery (numberOfQueries) -> World.multipleQueryContent random numberOfQueries |> Content.Json
+        | Success(Endpoints.MultipleQuery numberOfQueries)
+                                        -> World.multipleQueryContent random numberOfQueries >>= Content.Json
 
-        | Endpoints.MultipleQueryNoParam 
-        | Endpoints.MultipleQueryBadParam (_)       -> World.multipleQueryContent random 1 |> Content.Json
+        | MissingQueryParameter((Endpoints.MultipleQuery numberOfQueries), "queries")
+                                        -> World.multipleQueryContent random 1 >>= Content.Json
 
-        | Endpoints.DataUpdate    (numberOfQueries) -> World.multipleUpdateContent random numberOfQueries |> Content.Json
+        | Success(Endpoints.DataUpdate numberOfUpdates)
+                                        -> World.multipleUpdateContent random numberOfUpdates >>= Content.Json
 
-        | Endpoints.DataUpdateNoParam 
-        | Endpoints.DataUpdateBadParam (_)          -> World.multipleUpdateContent random 1 |> Content.Json
+        | MissingQueryParameter((Endpoints.DataUpdate numberOfUpdates), "updates") 
+                                        -> World.multipleUpdateContent random 1 >>= Content.Json
+        | _                             -> Content.NotFound
 
 #if WARP
 [<EntryPoint>]
